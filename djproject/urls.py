f048dfc7e592ad366875b9b5ec6a97eb2d85e481
huangyuan1 <wshy1121@qq.com>
2015-06-18 10:53:02 +0800

    制作第一个网页:Hello World

--- djproject/urls.py
+++ djproject/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from jobs.views import hello
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
 )
+
+
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def hello(req):
+	return HttpResponse("<h1>Hello World!</h1>")
+
