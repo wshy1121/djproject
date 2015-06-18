7fe16899a6f4cde8dc79fba205b9a9b480c6c583
huangyuan1 <wshy1121@qq.com>
2015-06-18 13:42:39 +0800

    模板初级使用

--- jobs/views.py
+++ jobs/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Template, Context
 import datetime
 
 def hello(req):
-	return HttpResponse("<h1>Hello World!</h1>")
+	t = Template("<h1>Hello World! My name is {{my_name}}.</h1>") 
+	c = Context({"my_name": "Stephane"})  
+	
+	return HttpResponse(t.render(c))
 
 def homepage(req):
 	return HttpResponse("<h1>this is homepage!</h1>")
