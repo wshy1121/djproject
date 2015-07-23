1abce7c8960aeec598f65cdcb57336730c48361c
huangyuan1 <wshy1121@qq.com>
2015-07-23 17:26:24 +0800

    创建数据库模型

--- djproject/settings.py
+++ djproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs',
+    'books',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,7 +67,7 @@ WSGI_APPLICATION = 'djproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'books.sqlite3'),
     }
 }
 
