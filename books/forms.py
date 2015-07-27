bc133f58470cbdfb0615c86dec24b0c80d05e696
huangyuan1 <wshy1121@qq.com>
2015-07-27 14:08:34 +0800

    添加message的校验：至少要四个单词

--- books/forms.py
+++ books/forms.py
@@ -10,3 +10,11 @@ class ContactForm(forms.Form):
 	topic = forms.ChoiceField(choices=TOPIC_CHOICES)
 	message = forms.CharField(widget=forms.Textarea(), initial="Replace with your feedback")
 	sender = forms.EmailField(required=False)
+
+	def clean_message(self):
+		message = self.cleaned_data.get('message', '')
+		num_words = len(message.split())
+		if num_words < 4:
+			raise forms.ValidationError("Not enough words!")
+		return message
+
