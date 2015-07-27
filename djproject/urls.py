aee14841b1927a17d09dd459453f98e3fab14dec
huangyuan1 <wshy1121@qq.com>
2015-07-27 14:58:24 +0800

    使用多个视图前缀:'books.views'和‘jobs.views’

--- djproject/urls.py
+++ djproject/urls.py
@@ -1,26 +1,29 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-from jobs.views import hello, homepage, ctime, template, current_datetime, current_datetime1
-from books.views import search, contact, add_publisher
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'djproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hello/$', hello),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', homepage),
-    url(r'^ctime/(\d{1,2})/$', ctime),
-    url(r'^template/$', template),
-    url(r'^current_datetime/$', current_datetime),
-    url(r'^current_datetime1/$', current_datetime1),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^search/$', search),
-    url(r'^contact/$', contact),
-    url(r'^add_publisher/$', add_publisher),
 )
 
+urlpatterns += patterns('books.views',
+    url(r'^search/$', 'search'),
+    url(r'^contact/$', 'contact'),
+    url(r'^add_publisher/$', 'add_publisher'),
+)
 
+urlpatterns += patterns('jobs.views',
+    url(r'^hello/$', 'hello'),
+    
+    url(r'^ctime/(\d{1,2})/$', 'ctime'),
+    url(r'^template/$', 'template'),
+    url(r'^current_datetime/$', 'current_datetime'),
+    url(r'^current_datetime1/$', 'current_datetime1'),
+
+)
