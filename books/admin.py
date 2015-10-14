3890e5ec3deffbd6fec2e2d96e76c2b66a11eaea
huangyuan1 <wshy1121@qq.com>
2015-10-14 17:21:43 +0800

    定制化Book的管理界面

--- books/admin.py
+++ books/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from books.models import Publisher, Author, Book  
 # Register your models here.
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publisher', 'publication_date')
+    ordering = ('-publication_date',)
+	#search_fields = ('title',)
   
 admin.site.register(Publisher)  
 admin.site.register(Author)  
-admin.site.register(Book)  
+admin.site.register(Book, BookAdmin) 
+
