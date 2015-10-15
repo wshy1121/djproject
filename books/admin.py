9160c945577f0b9db4cc85cb44e94b974638d90d
huangyuan1 <wshy1121@qq.com>
2015-10-15 13:56:54 +0800

    解决search_fields报错:前面tab改为4个空格

--- books/admin.py
+++ books/admin.py
@@ -6,8 +6,8 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'publisher', 'publication_date')
     list_filter = ('publisher', 'publication_date')
     ordering = ('-publication_date',)
-	#search_fields = ('title',)
-  
+    search_fields = ('title',)
+
 admin.site.register(Publisher)  
 admin.site.register(Author)  
 admin.site.register(Book, BookAdmin) 
