b55ef7ffb2aeeedfd67f6c38d6cbd8c55b07cf47
huangyuan1 <wshy1121@qq.com>
2015-10-16 16:29:17 +0800

    兼容python2.x和python3.x

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 # coding:utf-8
+from __future__ import unicode_literals
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
 class Article(models.Model):
-    title = models.CharField(u'标题', max_length=256)
-    content = models.TextField(u'内容')
-    pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable = True)
-    update_time = models.DateTimeField(u'更新时间',auto_now=True, null=True)
+    title = models.CharField('标题', max_length=256)
+    content = models.TextField('内容')
+    pub_date = models.DateTimeField('发表时间', auto_now_add=True, editable = True)
+    update_time = models.DateTimeField('更新时间',auto_now=True, null=True)
 
-    def __unicode__(self):# 在Python3中用 __str__ 代替 __unicode__
+    def __str__(self):
         return self.title
