5e43d9c181321fbdec9bd3bad61c4ffdc16cf0e8
huangyuan1 <wshy1121@qq.com>
2015-10-16 16:24:42 +0800

    让django后台显示 文章标题

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,6 @@ class Article(models.Model):
     content = models.TextField(u'内容')
     pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable = True)
     update_time = models.DateTimeField(u'更新时间',auto_now=True, null=True)
+
+    def __unicode__(self):# 在Python3中用 __str__ 代替 __unicode__
+        return self.title
